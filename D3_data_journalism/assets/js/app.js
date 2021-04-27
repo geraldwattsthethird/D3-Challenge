f3c08b985b536ce5a26da3f3b46cbed72d953b50
Gerald Watts <geraldwattsthethird@gmail.com>
2021-04-26 23:27:21 -0500

    Mon Update

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,3 +1,7 @@
+// Set width and height
+var svgWidth = 960;
+var svgHeight = 500;
+
 // Set default x and y axis variables.
 var xAxis = "poverty";
 var yAxis = "healthcare";
@@ -19,3 +23,57 @@ function yScale(data, chosenYAxis, chartHeight) {
         .range([chartHeight, 0]);
     return yLinearScale;
 }
+//Create makeResponsive function
+function makeResponsive() {
+    // Select div by id.
+    var svgArea = d3.select("#scatter").select("svg");
+    // Clear SVG data.
+    if (!svgArea.empty()) {
+        svgArea.remove();
+    }
+    // Define margins.
+    var margin = {
+        top: 50,
+        right: 50,
+        bottom: 100,
+        left: 80
+    };
+    // Chart the area minus margins.
+    var chartHeight = svgHeight - margin.top - margin.bottom;
+    var chartWidth = svgWidth - margin.left - margin.right;
+    // Create an SVG wrapper, append an SVG group that will hold our chart,
+    // and shift the latter by left and top margins.
+    var svg = d3
+    .select("#scatter")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+    // Append SVG group
+    var chartGroup = svg.append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+    d3.csv("assets/data/data.csv").then(function(demoData, err) {
+        if (err) throw err;
+        // Parse the data.
+        demoData.forEach(function(data) {
+            data.poverty = +data.poverty;
+            data.healthcare = +data.healthcare;
+            data.age = +data.age;
+            data.smokes = +data.smokes;
+            data.income = +data.income;
+            data.obesity = data.obesity;
+        });
+        // Create x and y linear scales.
+        var xLinearScale = xScale(demoData, xAxis, chartWidth);
+        var yLinearScale = yScale(demoData, yAxis, chartHeight);
+        // Create initial axis functions.
+        var bottomAxis = d3.axisBottom(xLinearScale);
+        var leftAxis = d3.axisLeft(yLinearScale);
+        // Append the x axis.
+        var xAxis = chartGroup.append("g")
+            .attr("transform", `translate(0, ${chartHeight})`)
+            .call(bottomAxis);
+
+
+
+    });
+};
